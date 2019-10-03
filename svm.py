e8b1d254581433cb7808c62bcf971ea3224df532
arthurhero <chenziwe@grinnell.edu>
2019-10-02 23:30:12 -0500

    added svm

---
+++ svm.py
@@ -0,0 +1,11 @@
+import utility as ut
+
+def classify_email(email_path):
+    '''
+    classifies an email to be spam or not
+    return 1 for spam, 0 for non-spam
+    '''
+
+if __name__ == '__main__':
+    result=classify_email('trec07p/data/inmail.1')
+    print(result)
